9ca93c5733677c8bfbf74a370d96a791efa33bca
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-12-05 14:31:00 +0100

    Primer Commit, layout menu creado

---
+++ app/src/main/java/com/example/menuresuelto/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.menuresuelto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Lógica del Menu de taskBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+}
